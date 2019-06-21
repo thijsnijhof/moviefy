1ddcb11602bf5d432db57fa9928e5b5847aa6b42
Thijs Nijhof <tnijhof91@gmail.com>
2019-06-21 15:31:52 +0100

    setup db and admin layouts

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/admin', 'AdminController@index')->name('admin');
+Route::get('/admin/posts', 'AdminPostsController@index')->name('admin_posts');
+Route::get('/admin/users', 'AdminUsersController@index')->name('admin_users');
+
+
+Route::get('/', 'HomeController@index')->name('home');
